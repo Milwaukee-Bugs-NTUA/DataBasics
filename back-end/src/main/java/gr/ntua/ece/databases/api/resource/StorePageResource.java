98d73fcf98f6dce52ab492306e69c30249420d5c
Efstathios Sarantinopoulos <ssarantin@gmail.com>
2020-06-15 13:45:31 +0300

    added to store userprofile resources PUT requests

--- back-end/src/main/java/gr/ntua/ece/databases/api/resource/StorePageResource.java
+++ back-end/src/main/java/gr/ntua/ece/databases/api/resource/StorePageResource.java
@@ -13,6 +13,9 @@ import java.lang.RuntimeException;
 import java.net.http.HttpRequest;
 import java.net.URLDecoder;
 
+import org.restlet.data.Form;
+import java.sql.Time;
+
 import gr.ntua.ece.databases.data.model.Store;
 
 import java.nio.charset.StandardCharsets;
@@ -20,6 +23,7 @@ import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.ToDoubleBiFunction;
 import java.util.Map;
 
 public class StorePageResource extends DatastoreResource {
@@ -36,6 +40,29 @@ public class StorePageResource extends DatastoreResource {
             Store result = dataAccess.fetchStorePage(storeId);
             return format.generateRepresentationStorePage(result);
         } 
+        catch (Exception e) {
+            throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
+        }
+
+    }
+
+    @Override
+    protected Representation put(Representation entity) throws ResourceException {
+
+        // Specify barcode of product
+        Form form = new Form(entity);
+        Integer size = Integer.valueOf(form.getFirstValue("Size"));
+        String addressCity = form.getFirstValue("Address City");
+        String addressStreet = form.getFirstValue("Address Street");
+        Integer addressNumnber = Integer.valueOf(form.getFirstValue("Address Number"));
+        String addressPostalCode = form.getFirstValue("Address Postal Code");
+        Time openingHour = Time.valueOf(form.getFirstValue("Opening Hour"));
+        Time closingHour = Time.valueOf(form.getFirstValue("Closing Hour"));
+
+        try {
+            dataAccess.updateStore(size,addressCity,addressStreet,addressNumnber,addressPostalCode,openingHour,closingHour);
+            return new JsonMapRepresentation(Map.of("status", "OK"));
+        }
         catch (Exception e) {
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
         }
--- back-end/src/main/java/gr/ntua/ece/databases/api/resource/UserProfileResource.java
+++ back-end/src/main/java/gr/ntua/ece/databases/api/resource/UserProfileResource.java
@@ -12,10 +12,13 @@ import org.restlet.resource.ServerResource;
 import java.lang.RuntimeException;
 import java.net.http.HttpRequest;
 import java.net.URLDecoder;
+import org.restlet.data.Form;
+
 
 import gr.ntua.ece.databases.data.model.User;
 
 import java.nio.charset.StandardCharsets;
+import java.sql.Date;
 import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
@@ -37,6 +40,36 @@ public class UserProfileResource extends DatastoreResource {
             User result = dataAccess.fetchUserProfile(userId);
             return format.generateRepresentationUserProfile(result);
         } 
+        catch (Exception e) {
+            throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
+        }
+
+    }
+
+    @Override
+    protected Representation put(Representation entity) throws ResourceException {
+
+        // Specify barcode of product
+        Form form = new Form(entity);
+        String email = form.getFirstValue("E-mail");
+        String firstName = form.getFirstValue("First Name");
+        String lastName = form.getFirstValue("Last Name");
+        Date dateOfBirth = Date.valueOf(form.getFirstValue("Date of Birth"));
+        String sex = form.getFirstValue("Sex");
+        String addressCity = form.getFirstValue("Address City");
+        String addressStreet = form.getFirstValue("Address Street");
+        Integer addressNumber = Integer.valueOf(form.getFirstValue("Address Number"));
+        String addressPostalCode = form.getFirstValue("Address Postal Code");
+        String phoneNumber = form.getFirstValue("Phone Number");
+        String maritalStatus = form.getFirstValue("Marital Status");
+        Integer numberOfChildren = Integer.valueOf(form.getFirstValue("Number of Children"));
+        Integer points = Integer.valueOf(form.getFirstValue("points"));
+
+
+        try {
+            dataAccess.updateUserProfile(email,firstName,lastName,dateOfBirth,sex,addressCity,addressStreet,addressNumber,addressPostalCode,phoneNumber,maritalStatus,numberOfChildren,points);
+            return new JsonMapRepresentation(Map.of("status", "OK"));
+        }
         catch (Exception e) {
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
         }
