fef0355e68cac1024c5930c9f6e9808e4732389b
Giannis Fakinos <giannhs.afasias@gmail.com>
2020-07-02 13:26:53 +0300

    Price History Done with table

--- frontend/src/app/components/price-history/price-history.component.ts
+++ frontend/src/app/components/price-history/price-history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-price-history',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./price-history.component.css']
 })
 export class PriceHistoryComponent implements OnInit {
+  history_table = null;
+  displayedColumnsT: string[] = ['StartingDate', 'EndingDate', 'Price'];
 
-  constructor() { }
+  constructor(private dataService: DataService,private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.dataService.sendGetRequest("product/" + params.get('barcode') + "/priceHistory").subscribe((data: any) => {
+          console.log(data);
+          this.history_table = data;
+      });
+  });
   }
 
 }
