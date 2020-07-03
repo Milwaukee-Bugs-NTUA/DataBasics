0b8f50473cab99816c5aa67ef21447b609873d13
Giannis Fakinos <giannhs.afasias@gmail.com>
2020-07-03 14:26:49 +0300

    Update Products Table Utility

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from './../../services/data.service';
+import { MatDialog } from '@angular/material/dialog';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { UpdateProductComponent } from '../update-product/update-product.component';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -10,8 +14,9 @@ import { DataService } from './../../services/data.service';
 export class ProductComponent implements OnInit {
     product = null;
     show_price_history = false;
+    new_product = null;
 
-  constructor(private dataService: DataService,private route: ActivatedRoute) { }
+  constructor(private dataService: DataService,private route: ActivatedRoute,public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -25,5 +30,38 @@ export class ProductComponent implements OnInit {
   onClickMe():void {
     this.show_price_history = true;
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(UpdateProductComponent, {
+        width: '250px',
+        height : 'auto',
+        data: new FormGroup({
+                Barcode: new FormControl({value:this.product.Barcode,disabled: true}, [Validators.required]),
+                Name: new FormControl(this.product.ProductName, [Validators.required]),
+                Brand: new FormControl(this.product.ProductBrandName, [Validators.required]),
+                Price: new FormControl({value:this.product.Price,disabled: true}, [Validators.required]),
+                CategoryId: new FormControl(this.product.CategoryId, [Validators.required]),
+              })
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+        console.log('Dialog Closed');
+        if (result !== undefined) {
+        this.new_product = result.value;
+        this.submitForm();
+        }
+    });
+  }
+    
+    submitForm(): void {
+        let httpParams = new HttpParams()
+                              .set('Name',this.new_product.Name)
+                              .set('Brand',this.new_product.Brand)
+                              .set('CategoryId',this.new_product.CategoryId);
+        this.dataService.sendPutRequest("product/" + this.product.Barcode + "/update",httpParams).subscribe(
+            (response) => {console.log(response);this.ngOnInit();},
+            (error) => console.log(error)
+        );
+    }
 
 }
--- frontend/src/app/components/update-product/update-product.component.ts
+++ frontend/src/app/components/update-product/update-product.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-update-product',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<UpdateProductComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: FormGroup) {
+    }
 
   ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
